024ba2b7beb568a76f676f743c201ded5e26354a
procgen <joshlorenzen49@gmail.com>
2018-04-21 16:55:14 -0700

    Camera button now opens camera to take a pic

--- java/com/example/josh/scavvy/MainActivity.java
+++ java/com/example/josh/scavvy/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.josh.scavvy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, 1);
+                }
             }
         });
     }
