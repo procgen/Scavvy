c5e1ddc74dcc8decf293d4bb787ec1bcb5983fe9
procgen <joshlorenzen49@gmail.com>
2018-04-21 21:08:10 -0700

    Pictures are saved to disk and loaded up for viewing

--- java/com/example/josh/scavvy/MainActivity.java
+++ java/com/example/josh/scavvy/MainActivity.java
@@ -1,22 +1,69 @@
 package com.example.josh.scavvy;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<ScavHunt> scavHunts = new ArrayList<ScavHunt>();
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_WRITE_PERMISSION = 2;
+
+    private File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "ScavvyPics");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d("ScavvyPics", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+//        File mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+//                "IMG_TEST" + ".jpg");
+        File mediaFile;
+//        try {
+            //mediaFile = File.createTempFile("IMG_TEST", ".jpg", mediaStorageDir);
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_TEST" + ".jpg");
+//        }
+//        catch (IOException e)
+//        {
+//            Log.d("ScavvyPics", "failed to create temp file");
+//            return null;
+//        }
+        return mediaFile;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +78,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, 1);
+                    File photoFile = null;
+                    photoFile = getOutputMediaFile();
+                    if(photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                                "com.example.android.fileprovider",
+                                photoFile);
+                        takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
                 }
             }
         });
@@ -46,6 +102,27 @@ public class MainActivity extends AppCompatActivity {
             btn.setTextSize(32);
             btn.setText(scavHunts.get(i).getName());
             yourlayout.addView(btn);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//            imageView.setImageBitmap(imageBitmap);
+            File imgFile = getOutputMediaFile();
+
+            if(imgFile.exists()){
+
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                ImageView myImage = (ImageView) findViewById(R.id.imageView);
+
+                myImage.setImageBitmap(myBitmap);
+
+            }
         }
     }
 
